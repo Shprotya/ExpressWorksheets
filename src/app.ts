56c27ca924dc81d572fd221f686bb9430f5edfc0
Lisa Kareeva <kareevaliza@gmail.com>
2026-09-16 10:32:43 +0100

    task 3 - added logging

--- src/app.ts
+++ src/app.ts
@@ -4,22 +4,28 @@ const PORT = process.env.PORT || 5000;
 
 const app: Application = express();
 
+// Middleware registered FIRST so it intercepts all incoming requests
+app.use((req, _res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+});
+
 app.get("/ping", async (_req: Request, res: Response) => {
   res.json({
     message: "hello from Lisa's server!",
   });
 });
 
-app.get('/bananas', async (_req : Request, res: Response) => { 
-    res.json({ 
-        message: "this is bananas", 
-    }); 
-}); 
+app.get("/bananas", async (_req: Request, res: Response) => {
+  res.json({
+    message: "this is bananas",
+  });
+});
 
-app.get('/havana', async (_req: Request, res: Response) => { 
-    res.json({ 
-    message: "havana, ooh na-na", 
-    }); 
+app.get("/havana", async (_req: Request, res: Response) => {
+  res.json({
+    message: "havana, ooh na-na",
+  });
 });
 
 app.listen(PORT, () => {
