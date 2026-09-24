50e26a6d8b877a71bbd45590de725a0a6dc537f6
Lisa Kareeva <kareevaliza@gmail.com>
2026-09-24 16:49:22 +0100

    ping route is not protected, but car routes are protected

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import { authenticateKey } from "./middleware/auth.middleware";
 const port = env.port;
 
 const app: Application = express();
-app.use(authenticateKey);
+app.use('/api/v1/cars', authenticateKey, carRoutes);
 
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use('/api/v1/cars', carRoutes);
